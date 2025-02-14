be365a88d807aae78dacffe5fe323490f9fc8da2
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-02-14 19:03:47 +0000

    Adding another items to react-admin, users section

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,12 +1,31 @@
-import { Datagrid, List, ReferenceField, TextField } from "react-admin";
+import {
+  Datagrid,
+  List,
+  ReferenceField,
+  TextField,
+  FunctionField,
+  useRecordContext,
+} from "react-admin";
+
+const PostPanel = () => {
+  const record = useRecordContext();
+  return <div>{record?.body}</div>;
+};
 
 const PostList = () => (
   <List>
-    <Datagrid>
+    <Datagrid
+      expand={<PostPanel />}
+      sx={{ ".RaDatagrid-headerCell": { padding: "16px" } }}
+    >
       <ReferenceField source="userId" reference="users" />
       <TextField source="id" />
-      <TextField source="title" />
-      <TextField source="body" />
+      <TextField source="title" label="Post Title" />
+      <FunctionField
+        label="Excerpt"
+        render={(record) => `${record.body.substring(0, 50)}...`}
+      />
+      <ReferenceField source="userId" reference="users" />
     </Datagrid>
   </List>
 );
